89290e9421982400a843cff07a5b0646a8fe7a2f
chaikovskyit <chaikovskyit@gmail.com>
2021-02-17 12:41:55 +0200

    connect router dom, create navbar, and two pages(home, about)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="container">
-      
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar />
+      <div className="container pt-4">
+        <Switch>
+          <Route path={'/'} exact component={Home}/>
+          <Route path={'/about'} component={About}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
